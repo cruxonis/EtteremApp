3f5bcc3af0fb67d96291fd8cdf15aa11014f85ab
cruxonis <tothbar@gmail.hu>
2021-02-08 15:10:51 +0100

    classes added

---
+++ database/migrations/2021_02_08_115512_create_foods_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->json('ingredients');
+            $table->text('description');
+            $table->json('categories');
+            $table->integer('point_id')->unsigned();
+            $table->foreign('point_id')->references('id')->on('points');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foods');
+    }
+}
